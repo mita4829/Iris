70bfac30c2b2e57d7401e0a2be0af213d44a1b57
Ross Blassingame <ross.blassingame@colorado.edu>
2017-04-22 13:43:15 -0600

    vibrate

---
+++ 4830_vibrateTests/_830_vibrateTests.swift
@@ -0,0 +1,36 @@
+//
+//  _830_vibrateTests.swift
+//  4830_vibrateTests
+//
+//  Created by Ross Blassingame on 4/22/17.
+//  Copyright © 2017 Ross Blassingame. All rights reserved.
+//
+
+import XCTest
+@testable import _830_vibrate
+
+class _830_vibrateTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
