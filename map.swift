0421cbd0d1fbb02a4deff15d79b73e58272558b4
Ross Blassingame <ross.blassingame@colorado.edu>
2017-04-22 19:20:48 -0600

    mapping function

---
+++ map.swift
@@ -0,0 +1,221 @@
+func choose(char: String) {
+
+	var a = 0, b = 0, c = 0, d = 0, e = 0, f = 0
+
+	if char == "a" | char == "1" {
+		a = 1
+		b = 0
+		c = 0
+		d = 0
+		e = 0
+		f = 0
+	}
+	if char == "b" | char == "2" {
+		a = 1
+		b = 0
+		c = 1
+		d = 0
+		e = 0
+		f = 0
+	}
+	if char == "c" | char == "3" {
+		a = 1
+		b = 1
+		c = 0
+		d = 0
+		e = 0
+		f = 0
+	}
+	if char == "d" | char == "4" {
+		a = 1
+		b = 1
+		c = 0
+		d = 1
+		e = 0
+		f = 0
+	}
+	if char == "e" | char == "5" {
+		a = 1
+		b = 0
+		c = 0
+		d = 1
+		e = 0
+		f = 0
+	}
+	if char == "f" | char == "6" {
+		a = 1
+		b = 1
+		c = 1
+		d = 0
+		e = 0
+		f = 0
+	}
+	if char == "g" | char == "7" {
+		a = 1
+		b = 1
+		c = 1
+		d = 1
+		e = 0
+		f = 0
+	}
+    if char == "h" | char == "8" {
+		a = 1
+		b = 0
+		c = 1
+		d = 1
+		e = 0
+		f = 0
+	}
+    if char == "i" | char == "9" {
+		a = 0
+		b = 1
+		c = 1
+		d = 0
+		e = 0
+		f = 0
+	}
+    if char == "j" | char == "0" {
+		a = 0
+		b = 1
+		c = 1
+		d = 1
+		e = 0
+		f = 0
+	}
+    if char == "k" {
+		a = 1
+		b = 0
+		c = 0
+		d = 0
+		e = 1
+		f = 0
+	}
+    if char == "l" {
+		a = 1
+		b = 0
+		c = 1
+		d = 0
+		e = 1
+		f = 0
+	}
+    if char == "m" {
+		a = 1
+		b = 1
+		c = 0
+		d = 0
+		e = 1
+		f = 0
+	}
+    if char == "n" {
+		a = 1
+		b = 1
+		c = 0
+		d = 1
+		e = 1
+		f = 0
+	}
+    if char == "o" {
+		a = 1
+		b = 0
+		c = 0
+		d = 1
+		e = 1
+		f = 0
+	}
+    if char == "p" {
+		a = 1
+		b = 1
+		c = 1
+		d = 0
+		e = 1
+		f = 0
+	}
+    if char == "q" {
+		a = 1
+		b = 1
+		c = 1
+		d = 1
+		e = 1
+		f = 0
+	}
+    if char == "r" {
+		a = 1
+		b = 0
+		c = 1
+		d = 1
+		e = 1
+		f = 0
+	}
+    if char == "s" {
+		a = 0
+		b = 1
+		c = 1
+		d = 0
+		e = 1
+		f = 0
+	}
+    if char == "t" {
+		a = 0
+		b = 1
+		c = 1
+		d = 1
+		e = 1
+		f = 0
+	}
+    if char == "u" {
+		a = 1
+		b = 0
+		c = 0
+		d = 0
+		e = 1
+		f = 1
+	}
+    if char == "v" {
+		a = 1
+		b = 0
+		c = 1
+		d = 0
+		e = 1
+		f = 1
+	}
+    if char == "w" {
+		a = 0
+		b = 1
+		c = 1
+		d = 1
+		e = 0
+		f = 1
+	}
+    if char == "x" {
+		a = 1
+		b = 1
+		c = 0
+		d = 0
+		e = 1
+		f = 1
+	}
+    if char == "y" {
+		a = 1
+		b = 1
+		c = 0
+		d = 1
+		e = 1
+		f = 1
+	}
+    if char == "z" {
+		a = 1
+		b = 0
+		c = 0
+		d = 1
+		e = 1
+		f = 1
+	}
+    if char == " " {
+		a = 0
+		b = 1
+		c = 0
+		d = 1
+		e = 1
+		f = 1
+	}
+}
